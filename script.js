822152e47d86ac9a75b9c7872615896fd444cb09
pundir8372 <pundirsahil320@gmail.com>
2024-07-08 17:37:19 +0530

    Updated Locomotive Scroll settings for mobile support

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ function locomotiveAnimation() {
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
+    smartphone: {
+      smooth: true,
+    },
+    tablet: {
+      smooth: true,
+    },
   });
+  
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
 
